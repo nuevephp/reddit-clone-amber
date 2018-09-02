63914d9ad13be464a6d844d01d918769213e2dca
Andrew Smith <a.smith@silentworks.co.uk>
2018-09-02 01:03:17 +0000

    Update the migration up and down sql

--- db/migrations/20180902005149899_add_user_id_to_posts.sql
+++ db/migrations/20180902005149899_add_user_id_to_posts.sql
@@ -1,9 +1,9 @@
 -- +micrate Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE "public"."posts" ADD COLUMN "user_id" BIGSERIAL;
-CREATE INDEX "user_id" ON "public"."posts" USING BTREE ("user_id");
+ALTER TABLE posts ADD COLUMN user_id BIGSERIAL;
+CREATE INDEX user_id ON posts USING BTREE (user_id);
 
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
-ALTER TABLE "public"."posts" DROP COLUMN "user_id";
-DROP INDEX "public"."user_id";
+DROP INDEX user_id;
+ALTER TABLE posts DROP COLUMN user_id;
