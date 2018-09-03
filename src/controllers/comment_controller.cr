152d157fba948d042d2bcbb89f1016598392d4f5
Andrew Smith <a.smith@silentworks.co.uk>
2018-09-03 23:05:36 +0000

    Update to add comment and delete comment

--- src/controllers/comment_controller.cr
+++ src/controllers/comment_controller.cr
@@ -6,39 +6,35 @@ class CommentController < ApplicationController
   end
 
   def create
-    post = Post.find! params[:link_id]
+    post = Post.find! params[:post_id]
     comment = Comment.new comment_params.validate!
     comment.post = post
     if user = current_user
       comment.user = user
     else
-      redirect_to action: :index, flash: {"danger" => "You need to be logged in"}
+      redirect_to controller: :post, action: :index, flash: {"danger" => "You need to be logged in"}
     end
     
     if comment.save
-      redirect_to action: :index, flash: {"success" => "Created comment successfully."}
+      redirect_back flash: {"success" => "Created comment successfully."}
     else
-      flash["danger"] = "Could not create Comment!"
-      render "new.slang"
+      redirect_back flash: {"danger" => "Could not create Comment!"}
     end
   end
 
   def destroy
     comment.destroy
-    redirect_back, flash: {"success" => "Deleted comment successfully."}
+    redirect_back flash: {"success" => "Deleted comment successfully."}
   end
 
   private def comment_params
     params.validation do
       required :post_id { |f| !f.nil? }
       required :body { |f| !f.nil? }
-      required :user_id { |f| !f.nil? }
     end
   end
 
   private def set_comment
-    # post = Post.find! params[:id]
-    # @comment = post.comments.new(comment_params)
     @comment = Comment.find! params[:id]
   end
 end
